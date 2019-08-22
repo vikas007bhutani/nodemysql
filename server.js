4b2cdce8de6efb34a46256be24345d0c39a006b0
vikas1982 <bhutani.vikas1982@hotmail.com>
2019-08-22 16:23:56 +0530

    commentl

--- server.js
+++ server.js
@@ -23,82 +23,82 @@ con.connect((err) => {
   console.log('Connection established');
 });
 
-var app = express()  
-app.use(bodyParser());  
-app.use(bodyParser.json({limit:'5mb'}));   
-app.use(bodyParser.urlencoded({extended:true}));  
+// var app = express()  
+// app.use(bodyParser());  
+// app.use(bodyParser.json({limit:'5mb'}));   
+// app.use(bodyParser.urlencoded({extended:true}));  
    
   
-app.use(function (req, res, next) {        
-     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4201');    
-     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');    
-     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');      
-     res.setHeader('Access-Control-Allow-Credentials', true);       
-     next();  
- });  
+// app.use(function (req, res, next) {        
+//      res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4201');    
+//      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');    
+//      res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');      
+//      res.setHeader('Access-Control-Allow-Credentials', true);       
+//      next();  
+//  });  
   
- var Schema = mongo.Schema;  
+//  var Schema = mongo.Schema;  
   
-var UsersSchema = new Schema({      
- name: { type: String   },       
- address: { type: String   },   
-},{ versionKey: false });  
+// var UsersSchema = new Schema({      
+//  name: { type: String   },       
+//  address: { type: String   },   
+// },{ versionKey: false });  
    
   
-var model = mongo.model('tracolearn', UsersSchema, 'tracolearn');  
+// var model = mongo.model('tracolearn', UsersSchema, 'tracolearn');  
   
-app.post("/api/SaveUser",function(req,res){   
- var mod = new model(req.body);  
- if(req.body.mode =="Save")  
- {  
-    mod.save(function(err,data){  
-      if(err){  
-         res.send(err);                
-      }  
-      else{        
-          res.send({data:"Record has been Inserted..!!"});  
-      }  
- });  
-}  
-else   
-{  
- model.findByIdAndUpdate(req.body.id, { name: req.body.name, address: req.body.address},  
-   function(err,data) {  
-   if (err) {  
-   res.send(err);         
-   }  
-   else{        
-          res.send({data:"Record has been Updated..!!"});  
-     }  
- });  
-  
-  
-}  
- })  
-  
- app.post("/api/deleteUser",function(req,res){      
-    model.remove({ _id: req.body.id }, function(err) {    
-     if(err){    
-         res.send(err);    
-     }    
-     else{      
-            res.send({data:"Record has been Deleted..!!"});               
-        }    
- });    
-   })  
+// app.post("/api/SaveUser",function(req,res){   
+//  var mod = new model(req.body);  
+//  if(req.body.mode =="Save")  
+//  {  
+//     mod.save(function(err,data){  
+//       if(err){  
+//          res.send(err);                
+//       }  
+//       else{        
+//           res.send({data:"Record has been Inserted..!!"});  
+//       }  
+//  });  
+// }  
+// else   
+// {  
+//  model.findByIdAndUpdate(req.body.id, { name: req.body.name, address: req.body.address},  
+//    function(err,data) {  
+//    if (err) {  
+//    res.send(err);         
+//    }  
+//    else{        
+//           res.send({data:"Record has been Updated..!!"});  
+//      }  
+//  });  
   
   
+// }  
+//  })  
   
- app.get("/api/getUser",function(req,res){  
-    model.find({},function(err,data){  
-              if(err){  
-                  res.send(err);  
-              }  
-              else{                
-                  res.send(data);  
-                  }  
-          });  
-  })  
+//  app.post("/api/deleteUser",function(req,res){      
+//     model.remove({ _id: req.body.id }, function(err) {    
+//      if(err){    
+//          res.send(err);    
+//      }    
+//      else{      
+//             res.send({data:"Record has been Deleted..!!"});               
+//         }    
+//  });    
+//    })  
+  
+  
+  
+//  app.get("/api/getUser",function(req,res){  
+//     model.find({},function(err,data){  
+//               if(err){  
+//                   res.send(err);  
+//               }  
+//               else{                
+//                   res.send(data);  
+//                   }  
+//           });  
+//   })  
   
   
 app.listen(8080, function () {  
