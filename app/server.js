d0fb30296745ea7dedc50d4ee0c25c2bca2e8f12
vikas1982 <bhutani.vikas1982@hotmail.com>
2019-08-25 21:58:59 +0530

    exp2

--- app/server.js
+++ app/server.js
@@ -35,7 +35,7 @@ app.use(bodyParser.urlencoded({extended:true}));
    
   
 app.use(function (req, res, next) {        
-     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');    
+     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');    
      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');    
      res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');      
      res.setHeader('Access-Control-Allow-Credentials', true);       
@@ -98,7 +98,7 @@ app.use(function (req, res, next) {
   res.status(200).send({
     success: 'true',
     message: 'todos retrieved successfully',
-    todos: db
+    todos: ''
         })
   });
   
