7c11f7ca3820cda2ca5bd2ac3fb334d078023db9
vikas1982 <bhutani.vikas1982@hotmail.com>
2019-08-27 23:39:20 +0530

    map

--- src/app/common.service.ts
+++ src/app/common.service.ts
@@ -12,10 +12,10 @@ export class CommonService {
   
   saveUser(user){      
     return this.http.post('http://172.31.38.163:8080/api/SaveUser/', user).pipe(map((response: Response) =>response.json()))              
-  }  
+  }   
   
   GetUser(){       
-    return this.http.get('http://172.31.38.163:8080/api/getUser/').pipe(map((response: Response) =>response.json()))              
+    return this.http.get('http://172.31.38.163:8080/api/getUser/').pipe(map(response =>response.json()))              
   }  
  deleteUser(id){   
     return this.http.post('http://172.31.38.163:8080/api/deleteUser/',{'id': id}).pipe(map((response: Response) =>response.json()))               
