12a422b0d422313a0c5820780aa6effa7681fbbd
vikas1982 <bhutani.vikas1982@hotmail.com>
2019-08-29 08:03:30 +0530

    headers

--- src/app/common.service.ts
+++ src/app/common.service.ts
@@ -14,8 +14,11 @@ export class CommonService {
     return this.http.post('http://172.31.38.163:8080/api/SaveUser/', user).pipe(map((response: Response) =>response.json()))              
   }   
   
-  GetUser(){       
-    return this.http.get('http://172.31.38.163:8080/api/getUser/').pipe(map((response: Response) =>response.json()))              
+  GetUser(){ 
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    headers.append('Accept', 'q= 0.8;application / json; q = 0.9');      
+    return this.http.get('http://172.31.38.163:8080/api/getUser/',{headers}).pipe(map((response: Response) =>response.json()))              
   }  
  deleteUser(id){   
     return this.http.post('http://172.31.38.163:8080/api/deleteUser/',{'id': id}).pipe(map((response: Response) =>response.json()))               
