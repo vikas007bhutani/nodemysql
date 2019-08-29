18977312d4eb03da5e5fc0f8bdc0fab5e55dff79
vikas1982 <bhutani.vikas1982@hotmail.com>
2019-08-29 10:36:59 +0530

    witherror

--- src/app/common.service.ts
+++ src/app/common.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';   
 import {Http,Response, Headers, RequestOptions } from '@angular/http';   
    
-import { Observable } from 'rxjs';  
-import { map } from 'rxjs/operators'; 
+import { Observable, of } from 'rxjs';  
+import { map, catchError } from 'rxjs/operators'; 
 
   
 @Injectable()  
@@ -18,7 +18,10 @@ export class CommonService {
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
     headers.append('Accept', 'q= 0.8;application / json; q = 0.9');      
-    return this.http.get('http://172.31.38.163:8080/api/getUser').pipe(map((response:any) =>response.json()))              
+    return this.http.get('http://172.31.38.163:8080/api/getUser').pipe(map((response:any) =>response.json()),catchError(<T>(error: any, result?: T) => {
+      console.log(error);
+      return of(result as T);
+    }))              
   }  
  deleteUser(id){   
     return this.http.post('http://172.31.38.163:8080/api/deleteUser/',{'id': id}).pipe(map((response: Response) =>response.json()))               
