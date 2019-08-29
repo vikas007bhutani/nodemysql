445863b831096b5fed4561cbbb98d472871126ac
vikas1982 <bhutani.vikas1982@hotmail.com>
2019-08-29 09:43:03 +0530

    final

--- src/app/common.service.ts
+++ src/app/common.service.ts
@@ -3,22 +3,23 @@ import {Http,Response, Headers, RequestOptions } from '@angular/http';
    
 import { Observable } from 'rxjs';  
 import { map } from 'rxjs/operators'; 
-
+import { HttpClient } from '@angular/common/http';
   
 @Injectable()  
 export class CommonService {  
   
-  constructor(private http: Http) { }  
+  constructor(private http: HttpClient) { }  
   
   saveUser(user){      
     return this.http.post('http://172.31.38.163:8080/api/SaveUser/', user).pipe(map((response: Response) =>response.json()))              
   }   
   
   GetUser<T>():Observable<any>{ 
-    let headers = new Headers();
-    headers.append('Content-Type', 'application/json');
-    headers.append('Accept', 'q= 0.8;application / json; q = 0.9');      
-    return this.http.get('http://172.31.38.163:8080/api/getUser',{headers}).pipe(map((response) =>response.json()))              
+    // let headers = new Headers();
+    // headers.append('Content-Type', 'application/json');
+    // headers.append('Accept', 'q= 0.8;application / json; q = 0.9');      
+    //return this.http.get('http://172.31.38.163:8080/api/getUser',{headers}).pipe(map((response) =>response.json()))              
+    return this.http.get('http://172.31.38.163:8080/api/getUser').pipe(map((response) =>response));             
   }  
  deleteUser(id){   
     return this.http.post('http://172.31.38.163:8080/api/deleteUser/',{'id': id}).pipe(map((response: Response) =>response.json()))               
