0c9a1dcaaea85cfae3e2d3921e78c3ff27775c69
vikas1982 <bhutani.vikas1982@hotmail.com>
2019-08-30 23:17:39 +0530

    httpclient

--- src/app/common.service.ts
+++ src/app/common.service.ts
@@ -4,27 +4,29 @@ import {ilogindetails } from '../app/model/loginmodel';
 import { Observable, of } from 'rxjs';  
 import { map, catchError } from 'rxjs/operators'; 
 
+import { HttpClient } from  "@angular/common/http";
+
   
 @Injectable()  
 export class CommonService {  
   
-  constructor(private http: Http) { }  
+  constructor(private httpclient: HttpClient) { }  
   
-  saveUser(user){      
-    return this.http.post('http://172.31.38.163:8080/api/SaveUser/', user).pipe(map((response: Response) =>response.json()))              
-  }   
+  // saveUser(user){      
+  //   return this.http.post('http://172.31.38.163:8080/api/SaveUser/', user).pipe(map((response: Response) =>response.json()))              
+  // }   
   
   GetUser<T>():Observable<any>{ 
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
     headers.append('Accept', 'q= 0.8;application / json; q = 0.9');      
-    return this.http.get('http://13.233.166.104:3000/api/getUser/').pipe(map((response:Response) =>response.json()),catchError(<T>(error: any, result?: T) => {
+    return this.httpclient.get('http://13.233.166.104:3000/api/getUser/').pipe(map((response:Response) =>response.json()),catchError(<T>(error: any, result?: T) => {
       console.log(error);
       return of(result as T);
     }))              
   }  
  deleteUser(id){   
-    return this.http.post('http://172.31.38.163:8080/api/deleteUser/',{'id': id}).pipe(map((response: Response) =>response.json()))               
+    return this.httpclient.post('http://172.31.38.163:8080/api/deleteUser/',{'id': id}).pipe(map((response: Response) =>response.json()))               
   }  
   
 } 
